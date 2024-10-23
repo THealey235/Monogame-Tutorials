f3c70b89c70fd4c51b0e7037b865df5606e7940e
THealey <tom.healey.235@gmail.com>
2024-10-23 12:50:54 +0100

    Sprite Follow Sprite
    Demonstrated how to make a sprite follow another.

---
+++ SpriteFollowSprite/Game1.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using SpriteFollowSprite.Sprites;
+using System.Collections.Generic;
+
+namespace SpriteFollowSprite
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private List<Sprite> _sprites;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            var texture = Content.Load<Texture2D>("Square");
+
+            var player = new Player(texture)
+            {
+                Colour = Color.Green,
+                Position = new Vector2(100, 100)
+            };
+
+            _sprites = new List<Sprite>()
+            {
+                player,
+                //Opt 1:
+                new Sprite(texture)
+                {
+                    Colour= Color.Blue,
+                    Position = new Vector2(200, 200)
+                }.SetFollowTarget(player, 75f),
+
+                //Opt 2:
+                new Sprite(texture)
+                {
+                    Colour= Color.Orange,
+                    Position = new Vector2(400, 200),
+                    FollowTarget = player,
+                    FollowDistance = 150f,
+                },
+            };
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            foreach (var sprite in _sprites)
+                sprite.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+
+            foreach (var sprite in _sprites)
+                sprite.Draw(gameTime, _spriteBatch);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ SpriteFollowSprite/Sprites/Sprite.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpriteFollowSprite.Sprites
+{
+    public class Sprite : Component
+    {
+        #region Members
+
+        protected float _layer {  get; set; }
+        protected Vector2 _origin { get; set; }
+        protected Vector2 _position { get; set; }
+        protected float _rotation { get; set; }
+
+        protected Texture2D _texture;
+
+        #endregion
+
+        #region Properites
+
+        public Color Colour { get; set; }
+
+        //The sprite that we want to follow
+        public Sprite FollowTarget { get; set; }
+
+        public float FollowDistance { get; set; }
+
+        public bool IsRemoved { get; set; }
+
+        public Vector2 Direction { get; set; }
+
+        public float RotationVelocity = 3f;
+
+        public float LinearVelocity = 4f;
+
+        public float Layer
+        {
+            get { return _layer; }
+            set
+            {
+                _layer = value;
+            }
+        }
+
+        public Vector2 Origin
+        {
+            get { return _origin; }
+            set
+            {
+                _origin = value;
+            }
+        }
+
+        public Vector2 Position
+        {
+            get { return _position; }
+            set
+            {
+                _position = value;
+            }
+        }
+
+        public Rectangle Rectangle
+        {
+            get 
+            {
+                return new Rectangle((int)Position.X - (int)Origin.X, (int)Position.Y - (int)Origin.Y, _texture.Width, _texture.Height);
+            }
+            
+        }
+
+        public float Rotaotion
+        {
+            get { return _rotation; }
+            set
+            {
+                _rotation = value;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public Sprite(Texture2D texture)
+        {
+            _texture = texture;
+
+            Origin = new Vector2(_texture.Width / 2, _texture.Height / 2);
+
+            Colour = Color.White;
+        }
+
+        public Sprite SetFollowTarget(Sprite followTarget, float followDistance)
+        {
+            FollowTarget = followTarget;
+
+            FollowDistance = followDistance;
+
+            return this;
+        }
+
+        protected void Follow()
+        {
+            if(FollowTarget == null) { return; }
+
+            var distance = FollowTarget.Position - this.Position;
+            _rotation = (float)Math.Atan2(distance.Y, distance.X);
+
+            Direction = new Vector2((float)Math.Cos(_rotation), (float)Math.Sin(_rotation));
+
+            var currentDistance = Vector2.Distance(this.Position, FollowTarget.Position);
+
+            if (currentDistance > FollowDistance)
+            {
+                //we want to move at a speed that does not get us too close
+                var t = MathHelper.Min((float)Math.Abs(currentDistance- FollowDistance), LinearVelocity); 
+                var velocity = Direction * t;
+
+                Position += velocity;
+            }
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, Position, null, Colour, _rotation, Origin, 1f, SpriteEffects.None, 0.1f);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            Follow();
+        }
+        #endregion
+    }
+}
